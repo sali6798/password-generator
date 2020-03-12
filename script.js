365cf258b3ba956fc3ab9edde48b409aceebf9ef
sali6798 <shaidee.alingcastre@gmail.com>
2020-03-11 21:31:54 -0700

    covered case where user wants to cancel prompt

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // asks user for password criteria and returns a char set based on that
 function getPasswordCriteria() {
-  var isLowercase = confirm("Do you want lowercase?");
-  var isUppercase = confirm("Do you want uppercase?");
+  var isLowercase = confirm("Do you want lowercase characters?");
+  var isUppercase = confirm("Do you want uppercase characters?");
   var isNumeric = confirm("Do you want numbers?");
   var isSpecialChars = confirm("Do you want special characters?");
 
@@ -41,25 +41,39 @@ function getPasswordCriteria() {
 // generates a password with the given char set
 function generatePassword() {
   var isValidPasswordLength = false;
+  var userInput = "";
   var passwordLength = 0;
-  
-  // Keeps asking for a length until the input given is valid
-  while (!isValidPasswordLength) {
-    passwordLength = parseInt(prompt("Choose a length between 8 and 128 characters for your password"));
+
+  // Keeps asking for a length until the input given is valid. If the user clicks
+  // cancel, stops asking and closes the prompt
+  while (userInput !== null && !isValidPasswordLength) {
+    userInput = prompt("Choose a length between 8 and 128 characters for your password");
+    passwordLength = parseInt(userInput);
     if (passwordLength >= 8 && passwordLength <= 128 ) {
+      console.log("inside");
       isValidPasswordLength = true;
+    }
+    else {
+      // if the user didn't click cancel alerts input is invalid
+      if (userInput !== null) {
+        alert("Not a valid length!");
+      }
     }
   }
   
-  var charSet = getPasswordCriteria();
   var password = "";
+  // if user clicks cancel on the prompt, it doesn't display anything in the textbox
+  if (userInput !== null) {
+    var charSet = getPasswordCriteria();
 
-  for (var i = 0; i < passwordLength; i++) {
-    // gets a random integer from 0 to charSet.length - 1
-    var random = Math.floor(Math.random() * charSet.length);
-    // gets char at position given by random from charSet and concatenates to the end of password
-    password += charSet.charAt(random);
+    for (var i = 0; i < passwordLength; i++) {
+      // gets a random integer from 0 to charSet.length - 1
+      var random = Math.floor(Math.random() * charSet.length);
+      // gets char at position given by random from charSet and concatenates to the end of password
+      password += charSet.charAt(random);
+    }
   }
+ 
 
   return password;
 }
